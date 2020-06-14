fc8eb4213bec929158c78b7427479e1e9a825227
Stella Enfield <stella.enfield@flightdataservices.com>
2020-06-14 15:13:39 +0100

    Add css file and edit movie list blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
         <title>{{$title}}</title>
     </head>
     <body>
@@ -19,16 +20,3 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 </html>
-<!--
-Create/update/delete customers (email address and customer name)
-Create/update/delete movies  (movie name)
-Create/update/delete showings (show time)
-Create/update/delete bookings (booking for showing tied to a customer, with multiple seat allocations)
-
-.gradient {
-  font-size: 72px;
-  background: -webkit-linear-gradient(#eee, #333);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
--->
